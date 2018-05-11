c87bc9e22d01824b2c76d2285fe2d556f304431a
Nicole Woodard <NicoleW@westcstdata.com>
2018-05-11 11:07:02 -0600

    Working version with morgan/bootstrap/chalk/debug

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const chalk = require('chalk')
+const debug = require('debug')('app') // Advanced debug loggin to terminal
+const morgan = require('morgan') // Print HTTP requests
+
+const path = require('path')
+
+let app = express()
+app.use(morgan('tiny')) // Display succint http requrests
+app.use(express.static(path.join(__dirname,'/public/'))) // Tell express where static files are
+
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist/umd')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'/views/index.html'))
+})
+
+app.listen(4000, () => {
+    debug('Server is running on ' + chalk.green('3000')) //TODO: why do `` template strings fail here?
+})
