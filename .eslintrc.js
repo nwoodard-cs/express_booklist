28fb3c49d989d7730aa9031166992ece6d4a2bd6
Nicole Woodard <NicoleW@westcstdata.com>
2018-05-15 08:05:54 -0600

    added bootstrap template

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
        "semi": [2, "never"],
-       "indent" : ["error", 4]
+       "indent" : ["error", 4],
+       "comma-dangle": ["error", "never"]
     }
 };
--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express')
-
-const chalk = require('chalk')
+const chalk = require('chalk') // Colored logging statements
 const debug = require('debug')('app') // Advanced debug loggin to terminal
 const morgan = require('morgan') // Print HTTP requests
-
 const path = require('path')
 
 const port = process.env.PORT || 3000
 const app = express()
-app.use(morgan('tiny')) // Display succint http requrests
-app.use(express.static(path.join(__dirname, '/public/'))) // Tell express where static files are
+const bookRouter = express.Router()
 
+app.use(morgan('tiny')) // Display succint http requests
+app.use(express.static(path.join(__dirname, '/public/'))) // Tell express where static files are
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
@@ -18,10 +17,25 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist
 
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
-// Set views engine
 
+bookRouter.route('/')
+    .get((req, res) => {
+        res.send('Hi books')
+    })
+bookRouter.route('/single')
+    .get((req, res) => {
+        res.send('Hi single book')
+    })
+
+app.use('/books', bookRouter)
 app.get('/', (req, res) => {
-    res.render('index', { list: ['a', 'b'], title: 'my special hell' })
+    res.render(
+        'index',
+        {
+            nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+            pageTitle: 'My Special Hell'
+        }
+    )
 })
 
 app.listen(port, () => {
